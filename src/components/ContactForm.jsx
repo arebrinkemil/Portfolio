083f31d061260fa0c935ce6e0221bafeb6052d9f
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-02-19 19:14:43 +0100

    pf

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PF from "../assets/PFbild.jpg";
 
 function ContactForm() {
   const onSubmit = async (event) => {
@@ -27,110 +28,139 @@ function ContactForm() {
   return (
     <div id="contact" className="flex items-center min-h-screen">
       <div className="container mx-auto">
-        <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
-          <div className="text-center">
-            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
-              Contact me
-            </h1>
-            <p className="text-gray-400 dark:text-gray-400">
-              Fill in the form to send me a message
-            </p>
+        <div className="max-w-[80vw] mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+          <div id="form-wrapper">
+            <div className="text-center">
+              <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact me
+              </h1>
+              <p className="text-gray-400 dark:text-gray-400">
+                Fill in the form to send me a message
+              </p>
+            </div>
+            <div className="m-7">
+              <form onSubmit={onSubmit}>
+                <input
+                  type="hidden"
+                  name="subject"
+                  value="New Submission from Web3Forms"
+                />
+                <input
+                  type="hidden"
+                  name="redirect"
+                  value="https://web3forms.com/success"
+                />
+                <input
+                  type="checkbox"
+                  name="botcheck"
+                  id=""
+                  style={{ display: "none" }}
+                />
+                <div className="mb-6">
+                  <label
+                    htmlFor="name"
+                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="John Doe"
+                    required
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="you@company.com"
+                    required
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="phone"
+                    className="text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="+1 (555) 1234-567"
+                    required
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="message"
+                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    Your Message
+                  </label>
+                  <textarea
+                    rows="5"
+                    name="message"
+                    id="message"
+                    placeholder="Your Message"
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                    required
+                  ></textarea>
+                </div>
+                <div className="mb-6">
+                  <button
+                    type="submit"
+                    className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                  >
+                    Send Message
+                  </button>
+                </div>
+                <p
+                  className="text-base text-center text-gray-400"
+                  id="result"
+                ></p>
+              </form>
+            </div>
           </div>
-          <div className="m-7">
-            <form onSubmit={onSubmit}>
-              <input
-                type="hidden"
-                name="subject"
-                value="New Submission from Web3Forms"
-              />
-              <input
-                type="hidden"
-                name="redirect"
-                value="https://web3forms.com/success"
-              />
-              <input
-                type="checkbox"
-                name="botcheck"
-                id=""
-                style={{ display: "none" }}
-              />
-              <div className="mb-6">
-                <label
-                  htmlFor="name"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="John Doe"
-                  required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+          <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div className="md:flex">
+              <div className="md:shrink-0">
+                <img
+                  className="h-48 w-full object-cover md:h-full md:w-48"
+                  src={PF}
+                  alt="Modern building architecture"
                 />
               </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+              <div className="p-8">
+                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                  Company retreats
+                </div>
+                <a
+                  href="#"
+                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
                 >
-                  Email Address
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="you@company.com"
-                  required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                />
+                  Incredible accommodation for your team
+                </a>
+                <p className="mt-2 text-slate-500">
+                  Looking to take your team away on a retreat to enjoy awesome
+                  food and take in some sunshine? We have a list of places to do
+                  just that.
+                </p>
               </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="phone"
-                  className="text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Phone Number
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  placeholder="+1 (555) 1234-567"
-                  required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="message"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Your Message
-                </label>
-                <textarea
-                  rows="5"
-                  name="message"
-                  id="message"
-                  placeholder="Your Message"
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                  required
-                ></textarea>
-              </div>
-              <div className="mb-6">
-                <button
-                  type="submit"
-                  className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
-                >
-                  Send Message
-                </button>
-              </div>
-              <p
-                className="text-base text-center text-gray-400"
-                id="result"
-              ></p>
-            </form>
+            </div>
           </div>
         </div>
       </div>
